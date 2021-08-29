c7148d56c72a8b29abaf4948b2091c409a829d7c
nakatuba <nakatuba0626@gmail.com>
2021-08-29 15:10:47 +0900

    Introduce mecab-ipadic-neologd

--- utils/tokenizer.py
+++ utils/tokenizer.py
@@ -3,7 +3,9 @@ import MeCab
 
 class MeCabTokenizer:
     def __init__(self):
-        self.tagger = MeCab.Tagger("-Owakati")
+        self.tagger = MeCab.Tagger(
+            "-Owakati -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd"
+        )
 
     def tokenize(self, text):
         text = self.tagger.parse(text)
