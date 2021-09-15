38ebd6c123f66ac50af32377d340f9cf2ecdc0ab
nakatuba <nakatuba0626@gmail.com>
2021-09-16 04:16:02 +0900

    Add script to make data

---
+++ make_data.sh
@@ -0,0 +1,17 @@
+cd data
+
+echo "Download wrime dataset"
+curl -O https://raw.githubusercontent.com/ids-cv/wrime/master/wrime.tsv
+# Fix column name
+if sed --version > /dev/null 2>&1; then
+  # Linux (GNU)
+  sed -i -e "1s/Saddness/Sadness/g" wrime.tsv
+else
+  # Mac (BSD)
+  sed -i "" -e "1s/Saddness/Sadness/g" wrime.tsv
+fi
+echo
+
+echo "Download word2vec"
+curl -O http://www.cl.ecei.tohoku.ac.jp/~m-suzuki/jawiki_vector/data/20170201.tar.bz2
+tar -xvf 20170201.tar.bz2
