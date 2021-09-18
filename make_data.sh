a85eaebb131b40f96d86b768583ceaba440fe1dd
nakatuba <nakatuba0626@gmail.com>
2021-09-19 06:31:09 +0900

    Update make_data.sh

--- make_data.sh
+++ make_data.sh
@@ -1,6 +1,6 @@
 cd data
 
-echo "Download wrime dataset"
+# Download wrime dataset
 curl -O https://raw.githubusercontent.com/ids-cv/wrime/master/wrime.tsv
 # Fix column name
 if sed --version > /dev/null 2>&1; then
@@ -10,14 +10,13 @@ else
   # Mac (BSD)
   sed -i "" -e "1s/Saddness/Sadness/g" wrime.tsv
 fi
-echo "Done\n"
+echo
 
-echo "Make labeled data"
+# Make labeled data
 python make_data.py
-echo "Done\n"
 
-echo "Download word2vec"
+# Download word2vec
 curl -O http://www.cl.ecei.tohoku.ac.jp/~m-suzuki/jawiki_vector/data/20170201.tar.bz2
+echo
 tar -xvf 20170201.tar.bz2
 python load_word2vec.py
-echo "Done"
