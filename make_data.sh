5414321c3fbd0c85891c0138e8100a612434c60c
nakatuba <nakatuba0626@gmail.com>
2021-10-31 23:04:38 +0900

    Change double quotes to single quotes

--- make_data.sh
+++ make_data.sh
@@ -5,10 +5,10 @@ curl -O https://raw.githubusercontent.com/ids-cv/wrime/master/wrime.tsv
 # Fix column name
 if sed --version > /dev/null 2>&1; then
   # Linux (GNU)
-  sed -i -e "1s/Saddness/Sadness/g" wrime.tsv
+  sed -i -e '1s/Saddness/Sadness/g' wrime.tsv
 else
   # Mac (BSD)
-  sed -i "" -e "1s/Saddness/Sadness/g" wrime.tsv
+  sed -i '' -e '1s/Saddness/Sadness/g' wrime.tsv
 fi
 echo
 
